243cd31ec9bdb3a8c2031fe9c6124842f873929d
justinestcourt <149642468+justinestcourt@users.noreply.github.com>
2024-07-16 13:04:51 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-// Function to change image source to GIF
+
 function changeToGif() {
     var image = document.getElementById('image');
-    image.src = 'images/DownloadGIF.gif'; // Replace 'image.gif' with your actual GIF file path
+    image.src = 'images/DownloadGIF.gif';
 }
 
-// Function to change image source back to original
+
 function changeToOriginal() {
     var image = document.getElementById('image');
-    image.src = 'images/DownloadText.png'; // Replace 'image.jpg' with your original image file path
+    image.src = 'images/DownloadText.png';
 }
